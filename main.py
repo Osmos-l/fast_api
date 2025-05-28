ff70662f18e3423b0683111a8da7d49c5948961e
Jordan LECLERCQ <emailjeux81@gmail.com>
2025-05-28 23:37:18 +0200

    CORS

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://jordanlcq.vercel.app",
+    "http://jordanlcq.vercel.app"
 ]
 
 app.add_middleware(
