35606b25bc77584e4b9fa027539e554e4aed6193
Jordan LECLERCQ <emailjeux81@gmail.com>
2025-05-29 13:44:13 +0200

    top 3 pred & new model

--- api/predict.py
+++ api/predict.py
@@ -11,6 +11,7 @@ class PredictRequest(BaseModel):
 
 class PredictResponse(BaseModel):
     prediction: int
+    top3: list[dict]
 
 model = MLP()
 
@@ -26,13 +27,20 @@ else:
 @router.post("/predict", response_model=PredictResponse)
 def predict(req: PredictRequest):
     if len(req.input) != 784:
-        return PredictResponse(prediction=-2)
+        return PredictResponse(prediction=-2, top3=[])
 
     if not model.ready:
-        return PredictResponse(prediction=-1)
+        return PredictResponse(prediction=-1, top3=[])
 
     X = np.array(req.input).reshape(1, -1).astype(np.float32)
     pred_probs = model.forward(X)
     pred_class = int(np.argmax(pred_probs, axis=1)[0])
+
+    # Calcul du top 3
+    top3_indices = np.argsort(pred_probs[0])[::-1][:3]
+    top3 = [
+        {"class": int(idx), "probability": float(pred_probs[0][idx])}
+        for idx in top3_indices
+    ]
     
-    return PredictResponse(prediction=pred_class)
+    return PredictResponse(prediction=pred_class, top3=top3)
